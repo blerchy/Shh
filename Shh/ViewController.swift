532df5023396a1f23551b6bb847797aec2edb607
Matt Lebl <matt@tinkerlab.io>
2016-04-20 20:33:12 -0700

    Redesigned Interface

--- Shh/ViewController.swift
+++ Shh/ViewController.swift
@@ -6,12 +6,16 @@
 //  Copyright © 2016 Matt Lebl. All rights reserved.
 //
 
+// Colour scheme: https://coolors.co/app/3d5a80-98c1d9-e0fbfc-ee6c4d-293241
+
 import UIKit
 import AudioKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var amplitudeIndicator: UIProgressView!
+    
+    @IBOutlet weak var buttonBackgroundView: UIView!
+    @IBOutlet weak var recordSwitch: UISwitch!
+    @IBOutlet weak var recordLabel: UILabel!
     
     var microphone = AKMicrophone()
     var delay: AKDelay?
@@ -22,15 +26,17 @@ class ViewController: UIViewController {
     var currentAmplitudeIndex = 0
     var greatestAmplitude: Double = 0.0
     
+    // MARK: UIView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        amplitudeIndicator.progress = 0
+        
+        setupViews()
         tracker = AKAmplitudeTracker(microphone)
     }
     
     override func viewDidAppear(animated: Bool) {
-        timer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: #selector(ViewController.measureAmplitude), userInfo: nil, repeats: true)
+//        timer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: #selector(ViewController.measureAmplitude), userInfo: nil, repeats: true)
         delay = AKDelay(tracker!, time: 0.2, dryWetMix: 1.0, feedback: 0)
         AudioKit.output = tracker!
         AKSettings.audioInputEnabled = true
@@ -43,52 +49,66 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // MARK: IBActions
 
     @IBAction func toggleJammer(sender: AnyObject) {
-        let button = sender as! UIButton
         if jamming {
             microphone.stop()
-            button.setTitle("Start Jamming", forState: .Normal)
             resetAmplitudeValues()
         } else {
             microphone.start()
-            button.setTitle("Stop Jamming", forState: .Normal)
         }
         jamming = !jamming
+    }
+    
+    @IBAction func buttonDown(sender: AnyObject) {
+        
+    }
+    @IBAction func buttonExit(sender: AnyObject) {
+        
+    }
+    @IBAction func buttonEnter(sender: AnyObject) {
+        
+    }
+    
+    // MARK: Helper functions
+    
+    func setupViews() {
+        buttonBackgroundView.layer.cornerRadius = buttonBackgroundView.bounds.width / 2
     }
     
     func resetAmplitudeValues() {
         lastAmplitudes = [0.0,0.0,0.0]
         currentAmplitudeIndex = 0
         greatestAmplitude = 0.0
-        amplitudeIndicator.progress = 0
     }
     
-    func measureAmplitude() {
-        if jamming {
-            lastAmplitudes[currentAmplitudeIndex] = tracker!.amplitude
-            var value: Double = 0.0
-            for amplitude in lastAmplitudes {
-                value += amplitude
-            }
-            value = value / 3
-            if value > greatestAmplitude {
-                greatestAmplitude = value
-            }
-            currentAmplitudeIndex += 1
-            if currentAmplitudeIndex >= 3 {
-                currentAmplitudeIndex = 0
-            }
-            
-            amplitudeIndicator.setProgress(Float(value / greatestAmplitude), animated: true)
-            
-            currentAmplitudeIndex += 1
-            
-            if currentAmplitudeIndex >= 3 {
-                currentAmplitudeIndex = 0
-            }
-        }
-    }
+//    func measureAmplitude() {
+//        if jamming {
+//            lastAmplitudes[currentAmplitudeIndex] = tracker!.amplitude
+//            var value: Double = 0.0
+//            for amplitude in lastAmplitudes {
+//                value += amplitude
+//            }
+//            value = value / 3
+//            if value > greatestAmplitude {
+//                greatestAmplitude = value
+//            }
+//            currentAmplitudeIndex += 1
+//            if currentAmplitudeIndex >= 3 {
+//                currentAmplitudeIndex = 0
+//            }
+//            
+//            amplitudeIndicator.setProgress(Float(value / greatestAmplitude), animated: true)
+//            
+//            currentAmplitudeIndex += 1
+//            
+//            if currentAmplitudeIndex >= 3 {
+//                currentAmplitudeIndex = 0
+//            }
+//        }
+//    }
 
 }
 
